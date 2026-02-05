d82c90335005506875c5d1221ffd9a389d90c854
vishnukumar <vishnu.spandi01@gmail.com>
2026-02-05 19:36:56 +0530

    All files added

---
+++ tests/ecofaz.spec.js
@@ -0,0 +1,11 @@
+import {test,expect} from "@playwright/test"
+
+import { EcoFzpage } from "../Pages/EcofzPage"
+
+test("ecoFz",async({page})=>{
+    const eco = new EcoFzpage(page);
+    await eco.visiturl()
+    await eco.image.click()
+    await eco.second.click();
+
+})
